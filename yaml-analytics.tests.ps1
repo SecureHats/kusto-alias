ed684960c4be5342f8e67486a9ccb6350fedac59
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-04-28 22:11:22 +0200

    Update yaml-analytics.tests.ps1

--- yaml-analytics.tests.ps1
+++ yaml-analytics.tests.ps1
@@ -34,7 +34,7 @@ Describe "Detections" {
             $query = (($yamlObject.query)) #-split "\n")
             foreach ($value in $aliasses) {
                 if ($query -match ($($value.command) + '\(')) {
-                    $value.command | Should -Match $value.alias -Because "'$($value.alias)()' is a deprecated value"
+                    $value.alias | Should -Match $value.command -Because "'$($value.command)()' is a deprecated value"
                 }
             }
         }
